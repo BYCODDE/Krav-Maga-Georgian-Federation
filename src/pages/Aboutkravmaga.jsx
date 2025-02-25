3add716834e3127b9352ada48fd89c0dff3ca4f5
BYCODDE <anaraliev217@gmail.com>
2025-02-25 16:17:21 +0400

    react routers added + header!

---
+++ src/pages/Aboutkravmaga.jsx
@@ -0,0 +1,5 @@
+function Aboutkravmaga() {
+  return <div>this is Aboutkravmaga page!</div>;
+}
+
+export default Aboutkravmaga;
---
+++ src/pages/Training.jsx
@@ -0,0 +1,5 @@
+function Training() {
+  return <div>this is Training page!</div>;
+}
+
+export default Training;
