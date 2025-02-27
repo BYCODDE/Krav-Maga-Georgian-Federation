f712d62f0a44cac976663c1a4b90342eedb3aa44
BYCODDE <anaraliev217@gmail.com>
2025-02-27 18:46:23 +0400

    footer structure added!

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,11 @@
-// import { ContactUs } from "..//ContactUs/ContactUs";
-// import ReusableTitle from "../ReusableTitle";
-// import SupportInfo from "../SupportInfo";
-
 function Footer() {
   return (
     <footer>
       <div className="px-[4.19rem] py-[3.15rem] bg-lightSkyBlue">
         <div className="flex flex-col gap-[5.86rem]">
-          {/* <ReusableTitle
-          // title={title}
-          // size={size}
-          // color={color}
-          // fontWeight={fontWeight}
-          /> */}
           <div className="flex flex-col gap-[4.5rem] md:flex-row md:justify-between md:gap-[8.19rem]">
-            <div className="md:w-1/2">
-              {/* <ContactUs /> */}
-            </div>
-            <div className="md:w-1/2">
-              {/* <SupportInfo /> */}
-            </div>
+            <div className="md:w-1/2"></div>
+            <div className="md:w-1/2"></div>
           </div>
         </div>
       </div>
