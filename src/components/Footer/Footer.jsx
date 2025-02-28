9c54b11b2b764ee3efe77fdb95d3174278a29d62
BYCODDE <anaraliev217@gmail.com>
2025-02-28 16:07:28 +0400

    all fixed!

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import ReusableTitle from "../ReusableTitle/ReusableTitle";
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { AiFillTikTok } from "react-icons/ai";
-import { FaLinkedin } from "react-icons/fa";
+
+import { socialLinks } from "../../lib/socialLinks";
+import { Link } from "react-router-dom";
+import { footerInfo } from "../../lib/footerInfo";
 
 function Footer() {
   return (
@@ -10,53 +10,28 @@ function Footer() {
       <div className="flex flex-row justify-between items-start gap-8">
         <div className="flex flex-col gap-[20px]">
           <ReusableTitle />
-          <p className=" text-[14px] lg:text-[20px] ">
-            Email: krav_maga_in_georgia@mail.ru
-          </p>
-          <p className=" text-[14px] lg:text-[20px] ">
-            Phone: (123) 456-7890
-          </p>
-          <p className=" text-[14px] lg:text-[20px] ">
-            Address: 2 University St, Tbilisi
-          </p>
+          {footerInfo.map((info) => (
+            <p key={info.id} className={`${info.style}`}>
+              {info.info}
+            </p>
+          ))}
         </div>
         <div className="flex flex-col items-center md:block md:flex-row md:items-start">
           <h3 className=" md:text-left text-[15px] lg:text-xl font-semibold mb-4 text-center">
             Follow Us
           </h3>
-          <div className="flex gap-0 md:flex-row md:gap-8 flex-col ">
-            <a
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-blue-600 px-3 py-1 lg:px-4 lg:py-2  text-2xl md:text-3xl"
-            >
-              <FaFacebook />
-            </a>
-            <a
-              href="https://www.tiktok.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-red-600 px-3 py-1 lg:px-4 lg:py-2  text-2xl md:text-3xl"
-            >
-              <AiFillTikTok />
-            </a>
-            <a
-              href="https://instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-pink-500 px-3 py-1 lg:px-4 lg:py-2  text-2xl md:text-3xl"
-            >
-              <FaInstagram />
-            </a>
-            <a
-              href="https://linkedin.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-blue-700 px-3 py-1 lg:px-4 lg:py-2  text-2xl md:text-3xl"
-            >
-              <FaLinkedin />
-            </a>
+          <div className="flex gap-0 md:flex-row md:gap-8 flex-col">
+            {socialLinks.map((link) => (
+              <Link
+                key={link.id}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={` ${link.style}`}
+              >
+                {<link.icon />}
+              </Link>
+            ))}
           </div>
         </div>
       </div>
---
+++ src/lib/footerInfo.js
@@ -0,0 +1,17 @@
+export const footerInfo = [
+  {
+    id: 1,
+    info: "Email: krav_maga_in_georgia@mail.ru",
+    style: "text-[14px] lg:text-[20px]",
+  },
+  {
+    id: 2,
+    info: "Phone: (123) 456-7890",
+    style: "text-[14px] lg:text-[20px]",
+  },
+  {
+    id: 3,
+    info: " Address: 2 University St, Tbilisi",
+    style: "text-[14px] lg:text-[20px]",
+  },
+];
