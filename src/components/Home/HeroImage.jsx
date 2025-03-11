993e716c1341b0b168c5898de9c87d1e0fa27bd3
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2025-03-11 16:15:34 +0400

    herocover translated!

--- src/components/Home/HeroImage.jsx
+++ src/components/Home/HeroImage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useGetHeroImage } from "../../hooks/useGetHeroImage";
+import { useTranslation } from "react-i18next";
 
 const HeroImage = () => {
   const { data, error, isError, isLoading } = useGetHeroImage();
+  const {t} = useTranslation();
 
   if (isLoading) return <p>Loading</p>;
   if (isError) return <p>{error.message}</p>;
@@ -14,7 +16,9 @@ const HeroImage = () => {
     >
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-center text-white px-4">
         <h1 className="text-4xl md:text-6xl font-bold">
-          Enhance your skills and master Krav Maga techniques
+          {t("hero_cover", {
+            defaultValue: "Enhance your skills and master Krav Maga techniques",
+          })}
         </h1>
         <button className="mt-6 bg-red-600 hover:bg-red-700 text-white font-semibold py-3 px-6 rounded-lg text-lg transition">
           Get Started
